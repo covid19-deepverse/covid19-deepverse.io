f300361449e8dce0330316d01aa1f9ffdfbf1d89
oat phattaraphon <phattaraphon.c@ku.th>
2020-11-26 02:02:46 +0700

    adding BarChart component

--- Frontend/src/components/BarChart.js
+++ Frontend/src/components/BarChart.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
+
 const BarChart = ({ data: { confirmed, recovered, deaths }, country }) => {
   const Data = {
     labels: ['Countries cases distribution'],
     datasets: [
       {
-        label: 'Infected',
-        backgroundColor: ['rgba(234, 87, 113, 1)',],
+        label: 'Confirmed',
+        backgroundColor: ['rgba(234, 87, 113, 1)'],
         data: [confirmed.value],
       },
       {
         label: 'Recovered',
-        backgroundColor: ['rgba(80, 227, 194, 1)',],
+        backgroundColor: ['rgba(80, 227, 194, 1)'],
         data: [recovered.value],
       },
       {
         label: 'Deaths',
-        backgroundColor: ['rgba(210, 45, 54, 1)',],
+        backgroundColor: ['rgba(210, 45, 54, 1)'],
         data: [deaths.value],
       },
     ],
@@ -24,6 +25,9 @@ const BarChart = ({ data: { confirmed, recovered, deaths }, country }) => {
 
   return (
     <>
+      <h1 className="title">
+        Infections History by Country <strong>{country}</strong>
+      </h1>
       <Bar
         data={Data}
         option={{
