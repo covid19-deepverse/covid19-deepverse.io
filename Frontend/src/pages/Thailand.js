ffde050148b6291956bb08e531d2d5024803357d
oat phattaraphon <phattaraphon.c@ku.th>
2020-11-26 01:46:59 +0700

    remove DonutChart from Thailand page

--- Frontend/src/pages/Thailand.js
+++ Frontend/src/pages/Thailand.js
@@ -4,13 +4,10 @@
  */
 
 import React from 'react';
-import DonutChart from '../components/DonutChart';
 
 function Thailand() {
   return (
     <div className="thailand">
-      <h1>Thailand</h1>
-      <DonutChart/>
     </div>
   );
 }
